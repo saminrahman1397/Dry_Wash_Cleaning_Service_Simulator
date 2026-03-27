02985e3f1f54bb84d44aaec0d6754c8bf347f3c0
Samin Rahman <saminrahman1397@gmail.com>
2026-03-27 22:34:51 +0600

    created FrontDeskReceptionist with getter/setters, Constructor

--- src/main/java/org/example/drywashcleaningservicesimulator/SaminRahman/Customer.java
+++ src/main/java/org/example/drywashcleaningservicesimulator/SaminRahman/Customer.java
@@ -12,6 +12,30 @@ public class Customer extends User {
         this.address = address;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Customer{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
     public void localDashBoard(){
 
     }
