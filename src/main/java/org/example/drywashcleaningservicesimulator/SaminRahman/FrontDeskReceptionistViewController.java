483b43ea6ef54a81d1eddca0392aa3f27722d313
SaminRahman <saminrahman1397@gmail.com>
2026-03-30 21:09:26 +0600

    Added Customer and Front Desk Receptionist View and controller class (haven't worked on the functionality yet)

---
+++ src/main/java/org/example/drywashcleaningservicesimulator/SaminRahman/FrontDeskReceptionistViewController.java
@@ -0,0 +1,37 @@
+package org.example.drywashcleaningservicesimulator.SaminRahman;
+
+import javafx.event.ActionEvent;
+
+public class FrontDeskReceptionistViewController {
+    @javafx.fxml.FXML
+    public void handOverOrderOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleDamageComplaintOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateOrderStatusOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createNewOrderOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void processPaymentOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void issueRefundOnActionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void printOrderSummaryOnActionButton(ActionEvent actionEvent) {
+    }
+}
