16b2e5b7c7ae7901df3a410080bd81f4211dfef8
Samin Rahman <saminrahman1397@gmail.com>
2026-04-10 20:48:19 +0600

    Scene switching between customerView and U1G2

--- src/main/java/org/example/drywashcleaningservicesimulator/SaminRahman/U1G2CancelOrderViewController.java
+++ src/main/java/org/example/drywashcleaningservicesimulator/SaminRahman/U1G2CancelOrderViewController.java
@@ -1,10 +1,15 @@
 package org.example.drywashcleaningservicesimulator.SaminRahman;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 public class U1G2CancelOrderViewController {
@@ -28,7 +33,11 @@ public class U1G2CancelOrderViewController {
     }
 
     @javafx.fxml.FXML
-    public void dashboardOnActionButton(ActionEvent actionEvent) {
+    public void dashboardOnActionButton(ActionEvent actionEvent) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/drywashcleaningservicesimulator/SaminRahmanFXML/CustomerView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) (((Node) actionEvent.getSource()).getScene().getWindow());
+        stage.setScene(scene);
     }
 
     @javafx.fxml.FXML
